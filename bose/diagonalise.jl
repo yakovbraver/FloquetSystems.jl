cd6b6af80016c402726153af34920aa52fc844fd
Yakov <jakovbraver@gmail.com>
2021-10-02 15:28:39 +0300

    Add Lattice and BoseHamiltonian structs

--- bose/diagonalise.jl
+++ bose/diagonalise.jl
@@ -6,20 +6,49 @@
 
 using SparseArrays, Combinatorics, KrylovKit
 
-"
-Return Hamiltonian
-    Ĥ = ∑ Cᵢⱼ â†ᵢ âⱼ,
-where Cᵢⱼ (i ≤ j) is the (i ↔ j) tunneling strength. Only the upper triangle of Ĉ is used.
-Also, return a vector of basis states on which Ĥ is defined.
-Number of cells in the lattice is determined automatically as max(j).
-"
-function constructH(C::AbstractSparseMatrix; nbozons::Integer)
-    C_rows, C_cols, C_vals = findnz(C)
-    ncells = maximum(C_cols)
-    H_rows, H_cols, H_vals = Int[], Int[], eltype(C_vals)[]
-    index_of_state, state_at_index = makebasis(ncells, nbozons)
-    for (state, index) in index_of_state
-        for (i, j, c) in zip(C_rows, C_cols, C_vals) # iterate over the terms of the Hamiltonian
+import Base.show
+
+"A type representing a lattice."
+struct Lattice{T}
+    J::SparseMatrixCSC{T} # tunneling strengths
+    ncells::Int           # number of cells
+    nbozons::Int          # number of bozons 
+    nstates::Int          # number of possible states (configurations)
+end
+
+"""
+Construct a `Lattice` object with `nbozons` bozons from the tunneling strengths matrix `J`.
+`J` is assumed to be hermitian, hence only upper triangular part will be used.
+Number of cells is determined automatically as the number of columns in `J`.
+"""
+function Lattice(J::AbstractSparseMatrix, nbozons::Integer)
+    ncells = size(J)[2]
+    Lattice(J, ncells, nbozons, binomial(nbozons+ncells-1, nbozons))
+end
+
+
+"A type representing a Bosonic Hamiltonian, Ĥ = ∑ 𝐽ᵢⱼ â†ᵢ âⱼ"
+mutable struct BoseHamiltonian{T}
+    lattice::Lattice{T}
+    H::SparseMatrixCSC{T} # the Hamiltonian matrix
+    states::Vector{Vector{Int}} # all possible states as a vector (index => state)
+    index_of_state::Dict{Vector{Int},Int} # a dictionary (state => index)
+end
+
+"Construct a `BoseHamiltonian` object defined on `lattice`."
+function BoseHamiltonian(lattice::Lattice)
+    bh = BoseHamiltonian(lattice, spzeros(eltype(lattice.J), 1, 1), Vector{Vector{Int}}(undef, lattice.nstates), Dict{Vector{Int},Int}())
+    makebasis!(bh)
+    constructH!(bh)
+    bh
+end
+
+"Construct the Hamiltonian matrix."
+function constructH!(bh::BoseHamiltonian)
+    J_rows, J_cols, J_vals = findnz(bh.lattice.J)
+    H_rows, H_cols, H_vals = Int[], Int[], eltype(J_vals)[]
+    for (state, index) in bh.index_of_state
+        for (i, j, c) in zip(J_rows, J_cols, J_vals) # iterate over the terms of the Hamiltonian
             if (state[j] > 0) # check that a particle is present at site `j` so that destruction âⱼ is possible
                 H_val = c * sqrt( (state[i]+1) * state[j] )
                 push!(H_vals, H_val)
@@ -28,7 +57,7 @@ function constructH(C::AbstractSparseMatrix; nbozons::Integer)
                 bra = copy(state)
                 bra[j] -= 1 
                 bra[i] += 1
-                H_row = index_of_state[bra]
+                H_row = bh.index_of_state[bra]
                 push!(H_rows, H_row)
                 # place the conjugate element into the transposed position
                 push!(H_vals, conj(H_val))
@@ -37,39 +66,41 @@ function constructH(C::AbstractSparseMatrix; nbozons::Integer)
             end
         end
     end
-    sparse(H_rows, H_cols, H_vals), state_at_index
+    bh.H = sparse(H_rows, H_cols, H_vals)
 end
-    
 
 """
-Generate all possible combinations of placing `nbozons` bozons to `ncells` cells.
-Return a dictionary (state => index) and a vector (index => state).
+Generate all possible combinations of placing the bozons in the lattice.
+Populate `bh.states` and `bh.index_of_state`.
 """
-function makebasis(ncells::Integer, nbozons::Integer)
-    index_of_state = Dict{Vector{Int}, Int}()
-    state_at_index = Vector{Vector{Int}}(undef, binomial(nbozons+ncells-1, nbozons))
+function makebasis!(bh::BoseHamiltonian)
     index = 1;
-    for partition in integer_partitions(nbozons)
-        length(partition) > ncells && continue # Example (nbozons = 3, ncells = 2): partition = [[3,0], [2,1], [1,1,1]] -- skip [1,1,1] as impossible
-        append!(partition, zeros(ncells-length(partition)))
-        for p in multiset_permutations(partition, ncells)
-            index_of_state[p] = index
-            state_at_index[index] = p
+    nb, nc = bh.lattice.nbozons, bh.lattice.ncells
+    for partition in integer_partitions(nb)
+        length(partition) > nc && continue # Example (nbozons = 3, ncells = 2): partition = [[3,0], [2,1], [1,1,1]] -- skip [1,1,1] as impossible
+        append!(partition, zeros(nc-length(partition)))
+        for p in multiset_permutations(partition, nc)
+            bh.index_of_state[p] = index
+            bh.states[index] = p
             index += 1
         end
     end
-    index_of_state, state_at_index
 end
 
-C = spzeros(4, 4)
-C[1, 2] = C[1, 3] = C[2, 4] = C[3, 4] = 1
-H, index = constructH(C, nbozons=2)
+"Print non-zero elements of the Hamiltonian `bh` in the format ⟨bra| Ĥ |ket⟩"
+function Base.show(io::IO, bh::BoseHamiltonian)
+    H_rows, H_cols, H_vals = findnz(bh.H)
+    for (i, j, val) in zip(H_rows, H_cols, H_vals) # iterate over the terms of the Hamiltonian
+        println(io, bh.states[i], " Ĥ ", bh.states[j], " = ", round(val, sigdigits=3))
+    end
+end
 
-A = spzeros(2, 2)
-A[1, 2] = 1
-H, index = constructH(A, nbozons=3)
+J = spzeros(4, 4)
+J[1, 2] = J[1, 3] = J[2, 4] = J[3, 4] = 1
+nbozons = 2
+lattice = Lattice(J, nbozons)
+bh = BoseHamiltonian(lattice)
 
-vals, vecs, info = eigsolve(H, 1, :SR)
+vals, vecs, info = eigsolve(bh.H, 1, :SR)
 
-findall(x -> abs(x) >= 1e-3, vecs[5])
-println(vecs[5])
+findall(x -> abs(x) >= 1e-3, vecs[1])
