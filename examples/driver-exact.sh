744834982f441679add0c4709294fef4908ebec9
Yakov <jakovbraver@gmail.com>
2024-03-17 18:46:21 +0200

    Add function

--- examples/driver-exact.sh
+++ examples/driver-exact.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+drive () {
+    for i in $(seq $nprocs); do
+        julia --project --check-bounds=no -t 1 -e 'include("examples/driver-exact.jl")' -- "$@" $N $nprocs $i &
+    done
+
+    wait
+
+    julia --project --check-bounds=no -t 1 -e 'include("examples/combine.jl")' -- "$@" $N 1 # last argument sets sort=true
+}
+
 nprocs=8
 N=240
 
-for i in $(seq $nprocs); do
-    julia --project --check-bounds=no -t 1 -e 'include("examples/driver-exact.jl")' -- 2 20 12 15 $N $nprocs $i &
-done
+touch times.txt
+echo "" >> times.txt
+date >> times.txt
 
-wait
-
-julia --project --check-bounds=no -t 1 -e 'include("examples/combine.jl")' -- 2 20 12 15 $N 1
+{ time drive 2 20 12 15; } 2>&1 | grep real >> times.txt
