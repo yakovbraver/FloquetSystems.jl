5db86af5f90a50f8947802e4b76bdd4378b881e4
Yakov <jakovbraver@gmail.com>
2024-03-20 12:50:06 +0200

    Update drivers

--- examples/driver-exact.jl
+++ examples/driver-exact.jl
@@ -1,4 +1,4 @@
-# A driving script for non-interactive calculation of the exact spectrum. Launch in multiprocessed mode as
+# A driving script for non-interactive calculation of the exact spectrum. Launch in multiprocess mode as
 #   $ julia --project --check-bounds=no -p 8 -e 'include("examples/driver-exact.jl")' -- f ω Umin Umax N
 # where `N` is the number of Us to scan.
 using FloquetSystems, DelimitedFiles
@@ -10,13 +10,14 @@ Us = range(Umin, Umax, N)
 
 J = 1.0f0
 U = 1
+sort = true
 
 # warm up
 lattice = Lattice(;dims=(1, 5), isperiodic=true)
 bh = BoseHamiltonian(lattice, J, U, f, ω)
 outdir = "f$(f)_w$(ω)_U$(Umin)-$(Umax)_$(lattice.dims[1])x$(lattice.dims[2])-exact"
 
-quasienergy(bh, Us, sort=false, showprogress=true);
+quasienergy(bh, Us; sort);
 
 # actual calculation
 lattice = Lattice(;dims=(2, 4), isperiodic=true)
@@ -25,12 +26,12 @@ bh = BoseHamiltonian(lattice, J, U, f, ω)
 
 nstates = length(lattice.basis_states)
 
-ε, sp = quasienergy(bh, Us, sort=false, showprogress=true);
+ε, sp = quasienergy(bh, Us; sort);
 
 open(outdir*".txt", "w") do io
     writedlm(io, vcat(Us', ε))
 end
 
-open(outdir*"-perm.txt", "w") do io
+sort && open(outdir*"-perm.txt", "w") do io
     writedlm(io, sp)
 end
